986ad3a7f47c110e0fa111da5a9fae17f0c44e7b
Graziela Antunes <graziela.atns@gmail.com>
2023-01-27 17:42:13 -0300

    switch exec_mode: 'fork'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: "foods-explorer",
     script: "./src/server.js",
-    exec_mode : "cluster",
+    exec_mode : "fork",
     env: {
       NODE_ENV: "development",
     },
